4ed3105d4b02e68d9b49d9f0334ee4b991a42835
MechCoder0 <30991841+MechCoder0@users.noreply.github.com>
2017-12-02 14:19:18 -0500

    Update Values.java

--- Values.java
+++ Values.java
@@ -1,6 +1,8 @@
 package distributedSystems;
 
 //This is the object that we will be passing around
+//This object has 2 different field, request and reply
+//They are acccessed through the methods, this is called encapsulation 
 public class Values {
 	private boolean request = false, reply = false;
 
